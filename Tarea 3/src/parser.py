8585405c0c3ab665ad5af6d46e4b9e0516270b12
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-11-23 21:37:44 -0300

    Test and documentation

--- Tarea 3/src/parser.py
+++ Tarea 3/src/parser.py
@@ -1,4 +1,3 @@
-import sys
 from collections import namedtuple
 from numbers import Number
 
@@ -7,6 +6,20 @@ from sexpdata import loads, Symbol
 
 from src.Exceptions import WrongSyntaxException, FreeIdentifier
 
+"""
+Expr
+
+<s-expr> ::= <num>
+         | (+ <s-expr> <s-expr>)
+         | (- <s-expr> <s-expr>)
+         | (if0 <s-expr> <s-expr> <s-expr>)
+         | (with (<id> <s-expr>) <s-expr>)
+         | <id>
+         | (fun (<id>) <s-expr>)
+         | (<s-expr> <s-expr>)
+         | (seqn <s-expr> <s-expr>)
+         | (set <id> <s-expr>)
+"""
 Num = namedtuple('num', 'n')
 Id = namedtuple('id', 's')
 Add = namedtuple('add', ['l', 'r'])
@@ -17,19 +30,61 @@ Fun = namedtuple('fun', ['id', 'body'])
 Seqn = namedtuple('seqn', ['exp1', 'exp2'])
 Set = namedtuple('set', ['id', 'exp'])
 
-NumV = namedtuple('numV', ['n'])
-ClosureV = namedtuple('closureV', ['id', 'body', 'env'])
+"""
+Env
 
+<env> ::= mtEnv()
+        | aEnv(<id>, <loc>, <env>)
+"""
 MTEnv = namedtuple('mtEnv', [])
 AEnv = namedtuple('aEnv', ['id', 'loc', 'env'])
 
+"""
+Values
+
+<val> ::= NumV(<num>)
+        | ClosureV(<id>, <expr>, <env>)
+"""
+NumV = namedtuple('numV', ['n'])
+ClosureV = namedtuple('closureV', ['id', 'body', 'env'])
+
+"""
+Sto
+
+<sto> ::= mtSto()
+        | aSto(<loc>, <val>, <sto>)
+<loc> ::= number
+"""
 MTSto = namedtuple('mtSto', [])
 ASto = namedtuple('aSto', ['loc', 'val', 'sto'])
 
+"""
+Val*Sto
+
+<vs> ::= ValStore(<val>, <sto>)
+"""
 ValStore = namedtuple('v_s', ['val', 'sto'])
 
 
 def parse(s_expr):
+    """
+    parser :: String -> Expr
+
+    Parses a string representing an s-expr and transforms it to ADT.
+
+    where String is represented by the following:
+    <s-expr> ::= <num>
+             | (+ <s-expr> <s-expr>)
+             | (- <s-expr> <s-expr>)
+             | (if0 <s-expr> <s-expr> <s-expr>)
+             | (with (<id> <s-expr>) <s-expr>)
+             | <id>
+             | (fun (<id>) <s-expr>)
+             | (<s-expr> <s-expr>)
+             | (seqn <s-expr> <s-expr>)
+             | (set <id> <s-expr>)
+    """
+
     def parser(arg_list):
 
         if isinstance(arg_list, Number):
@@ -75,10 +130,19 @@ def parse(s_expr):
 
 
 def error(identifier):
+    """
+    error :: <id> -> Exception
+    Just matches a FreeIdentifier exception with an identifier.
+    """
     raise FreeIdentifier(identifier)
 
 
 def env_lookup(x, env):
+    """
+    env-lookup :: Sym Env -> Loc
+    Searches for a symbol x in an environment and returns it's location id.
+    Raises an error if not found.
+    """
     match = Matcher([
         ('MTEnv()', lambda: error(x)),
         ('AEnv(iid, loc, rest)', lambda iid, loc, rest: loc if iid == x else env_lookup(x, rest)),
@@ -87,6 +151,11 @@ def env_lookup(x, env):
 
 
 def sto_lookup(l, sto):
+    """
+    sto-lookup :: Loc Sto -> Val
+    Searches for a location l in a storage and returns it's associated value.
+    Raises an error if not found.
+    """
     match = Matcher([
         ('MTSto()', lambda: error(l)),
         ('ASto(loc, val, rest)', lambda loc, val, rest: val if loc == l else sto_lookup(l, rest)),
@@ -95,6 +164,11 @@ def sto_lookup(l, sto):
 
 
 def loc_lookup(val, sto):
+    """
+    sto-lookup :: Val Sto -> Loc
+    Searches for a value val in a storage and returns it's associated location id.
+    Raises an error if not found.
+    """
     match = Matcher([
         ('MTSto()', lambda: error(val)),
         ('ASto(loc, s_val, rest)', lambda loc, s_val, rest: loc if s_val == val else loc_lookup(val, rest)),
@@ -103,6 +177,10 @@ def loc_lookup(val, sto):
 
 
 def next_loc(sto):
+    """
+    next_loc :: Sto -> Int
+    Calculates the next available location id in a storage.
+    """
     match = Matcher([
         ('MTSto()', lambda: 0),
         ('ASto(_, _, rest)', lambda rest: 1 + next_loc(rest)),
@@ -110,69 +188,83 @@ def next_loc(sto):
     return match(sto)
 
 
-def interp(expr, env, sto):
-    def num_plus(n1, n2):
-        return NumV(n1.n + n2.n)
+def interp(expr, env):
+    """
+    interp :: Expr x Env -> Val
+    Evaluates a program withing an environment and returns the result of the evaluation.
+    """
 
-    def num_sub(n1, n2):
-        return NumV(n1.n - n2.n)
+    def interp_rec(r_expr, r_env, sto):
 
-    def do_sum(l, r, s_env, s_sto):
-        l_vs = interp(l, s_env, s_sto)
-        r_vs = interp(r, s_env, l_vs.sto)
-        return ValStore(num_plus(l_vs.val, r_vs.val), r_vs.sto)
+        def num_plus(n1, n2):
+            return NumV(n1.n + n2.n)
 
-    def do_sub(l, r, s_env, s_sto):
-        l_vs = interp(l, s_env, s_sto)
-        r_vs = interp(r, s_env, l_vs.sto)
-        return ValStore(num_sub(l_vs.val, r_vs.val), r_vs.sto)
+        def num_sub(n1, n2):
+            return NumV(n1.n - n2.n)
 
-    def do_if(c, t, f, s_env, s_sto):
-        c_vs = interp(c, s_env, s_sto)
-        if c_vs.val.n == 0:
-            return interp(t, s_env, c_vs.sto)
-        else:
-            return interp(f, s_env, c_vs.sto)
+        def do_sum(l, r, s_env, s_sto):
+            l_vs = interp_rec(l, s_env, s_sto)
+            r_vs = interp_rec(r, s_env, l_vs.sto)
+            return ValStore(num_plus(l_vs.val, r_vs.val), r_vs.sto)
 
-    def do_app(f_exp, arg, s_env, s_sto):
-        f_vs = interp(f_exp, s_env, s_sto)
-        a_vs = interp(arg, s_env, f_vs.sto)
-        new_loc = next_loc(a_vs.sto)
-        return interp(f_vs.val.body,
-                      AEnv(f_vs.val.id, new_loc, f_vs.val.env),
-                      ASto(new_loc, a_vs.val, a_vs.sto))
+        def do_sub(l, r, s_env, s_sto):
+            l_vs = interp_rec(l, s_env, s_sto)
+            r_vs = interp_rec(r, s_env, l_vs.sto)
+            return ValStore(num_sub(l_vs.val, r_vs.val), r_vs.sto)
 
-    def do_seq(exp1, exp2, s_env, s_sto):
-        vs_1 = interp(exp1, s_env, s_sto)
-        return interp(exp2, s_env, vs_1.sto)
+        def do_if(c, t, f, s_env, s_sto):
+            c_vs = interp_rec(c, s_env, s_sto)
+            if c_vs.val.n == 0:
+                return interp_rec(t, s_env, c_vs.sto)
+            else:
+                return interp_rec(f, s_env, c_vs.sto)
 
-    def do_set(the_id, exp, s_env, s_sto):
-        i_vs = interp(the_id, s_env, s_sto)
-        loc = loc_lookup(i_vs.val, s_sto)
-        e_vs = interp(exp, s_env, i_vs.sto)
-        return ValStore(e_vs.val, ASto(loc, e_vs.val, e_vs.sto))
+        def do_app(f_exp, arg, s_env, s_sto):
+            f_vs = interp_rec(f_exp, s_env, s_sto)
+            a_vs = interp_rec(arg, s_env, f_vs.sto)
+            new_loc = next_loc(a_vs.sto)
+            return interp_rec(f_vs.val.body,
+                              AEnv(f_vs.val.id, new_loc, f_vs.val.env),
+                              ASto(new_loc, a_vs.val, a_vs.sto))
 
-    match = Matcher([
-        ('Num(n)', lambda n: ValStore(NumV(n), sto)),
-        ('Add(l, r)', lambda l, r: do_sum(l, r, env, sto)),
-        ('Sub(l, r)', lambda l, r: do_sub(l, r, env, sto)),
-        ('Fun(iid, body)', lambda iid, body: ValStore(ClosureV(iid, body, env), sto)),
-        ('IF0(c, t, f)', lambda c, t, f: do_if(c, t, f, env, sto)),
-        ('Id(s)', lambda s: ValStore(sto_lookup(env_lookup(s, env), sto), sto)),
-        ('App(f_exp, arg)', lambda f_exp, arg: do_app(f_exp, arg, env, sto)),
-        ('Seqn(exp1, exp2)', lambda exp1, exp2: do_seq(exp1, exp2, env, sto)),
-        ('Set(iid, exp)', lambda iid, exp: do_set(iid, exp, env, sto)),
-    ])
+        def do_seq(exp1, exp2, s_env, s_sto):
+            vs_1 = interp_rec(exp1, s_env, s_sto)
+            return interp_rec(exp2, s_env, vs_1.sto)
 
-    try:
-        return match(expr)
-    except:
-        raise
+        def do_set(the_id, exp, s_env, s_sto):
+            i_vs = interp_rec(the_id, s_env, s_sto)
+            loc = loc_lookup(i_vs.val, s_sto)
+            e_vs = interp_rec(exp, s_env, i_vs.sto)
+            return ValStore(e_vs.val, ASto(loc, e_vs.val, e_vs.sto))
+
+        match = Matcher([
+            ('Num(n)', lambda n: ValStore(NumV(n), sto)),
+            ('Add(l, r)', lambda l, r: do_sum(l, r, r_env, sto)),
+            ('Sub(l, r)', lambda l, r: do_sub(l, r, r_env, sto)),
+            ('Fun(iid, body)', lambda iid, body: ValStore(ClosureV(iid, body, r_env), sto)),
+            ('IF0(c, t, f)', lambda c, t, f: do_if(c, t, f, r_env, sto)),
+            ('Id(s)', lambda s: ValStore(sto_lookup(env_lookup(s, r_env), sto), sto)),
+            ('App(f_exp, arg)', lambda f_exp, arg: do_app(f_exp, arg, r_env, sto)),
+            ('Seqn(exp1, exp2)', lambda exp1, exp2: do_seq(exp1, exp2, r_env, sto)),
+            ('Set(iid, exp)', lambda iid, exp: do_set(iid, exp, r_env, sto)),
+        ])
+
+        try:
+            return match(r_expr)
+        except:
+            raise
+
+    return interp_rec(expr, env, MTSto())
 
 
 def run(prog):
+    """
+    run :: String -> String
+    Evaluates a program as a string and results the resulting value as a string.
+    If an error occurs, it will be displayed as a string.
+    """
     try:
-        res = interp(parse(prog), MTEnv(), MTSto())
+        res = interp(parse(prog), MTEnv())
         match = Matcher([
             ('NumV(n)', lambda n: n),
             ('ClosureV(_, _, _)', lambda: 'Function'),
@@ -182,11 +274,10 @@ def run(prog):
 
     except FreeIdentifier as e:
         print("error: identificador libre!! {}".format(e))
-    except Exception as e:
+    except:
         print("error: expresión inválida {}".format(prog))
-        print(e)
     exit(1)
 
 
 if __name__ == "__main__":
-    run(sys.argv[1])
+    print(run("(8 10)"))
--- Tarea 3/test/test_parse.py
+++ Tarea 3/test/test_parse.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
-from src.parser import parse, Num, Add, App, Fun, Id, Sub, IF0
+
+from src.parser import parse, Num, Add, App, Fun, Id, Sub, IF0, Seqn, Set
 
 
 class TestParse(TestCase):
     def test_parse_num(self):
         expr = parse("4")
         expected = Num(4)
+        self.assertEqual(expr, expected)
+
+    def test_parse_id(self):
+        expr = parse("x")
+        expected = Id('x')
         self.assertEqual(expr, expected)
 
     def test_parse_sum(self):
@@ -35,5 +41,32 @@ class TestParse(TestCase):
 
     def test_parse_app(self):
         expr = parse("((fun (x) (+ 1 x)) 4)")
-        expected = App((Fun('x', Add(Num(1), Id('x'))), Num(4)))
+        expected = App(Fun('x', Add(Num(1), Id('x'))), Num(4))
+        self.assertEqual(expr, expected)
+
+    def test_parse_enun_fun(self):
+        expr = parse("(fun (x) x)")
+        expected = Fun('x', Id('x'))
+        self.assertEqual(expr, expected)
+
+    def test_parse_inval(self):
+        expr = parse("(8 10)")
+        expected = App(Num(8), Num(10))
+        self.assertEqual(expr, expected)
+
+    def test_parse_enun_fun2(self):
+        expr = parse("(((fun (x) x) (fun (x) (+ x 5))) 3)")
+        expected = App(App(Fun('x', Id('x')), Fun('x', Add(Id('x'), Num(5)))), Num(3))
+        self.assertEqual(expr, expected)
+
+    def test_parse_enun_with(self):
+        expr = parse("(with (x 3) (with (f (fun (y) (+ x y))) (with (x 5) (f 4))))")
+        expected = App(
+            Fun('x', App(Fun('f', App(Fun('x', App(Id('f'), Num(4))), Num(5))), Fun('y', Add(Id('x'), Id('y'))))),
+            Num(3))
+        self.assertEqual(expr, expected)
+
+    def test_parse_enun_setseqn(self):
+        expr = parse("(with (x 3) (+ (seqn (set x 5) x) x))")
+        expected = App(Fun('x', Add(Seqn(Set(Id('x'), Num(5)), Id('x')), Id('x'))), Num(3))
         self.assertEqual(expr, expected)
